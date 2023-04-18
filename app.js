7b98d3f12c8fdc331bfb5b1a184e4417a85d1f38
Nicky <nicholasaahlman@gmail.com>
2023-04-18 00:05:52 -0500

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 const express = require("express");
 const app = express();
+// const morgan=require ("morgan")
+const {list, find}=require("./postBank")
+// const static=require("static")
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.use(morgan('dev'));
+app.use(express.static('public'))
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+const posts=list();
+
+const html=`
+<html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+      <a href="/posts/${post.id}">${post.title}</a>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
+
+
+res.send(html)
+});
+
+app.get('/posts/:id', (req, res)=>{
+  const id=req.params.id 
+  const post=find(id);
+  if (!post.id) {
+    // If the post wasn't found, just throw an error
+    throw new Error('Not Found')
+  }
+  const html=  `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+            ${post.content}
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`
+  res.send(html)
+})
+
+const { PORT = 1338 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
